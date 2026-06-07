7d9d531d88bb77a05808a2a63870d7422998c579
Marko Mijakovski <m.mijakovski07@gmail.com>
2026-06-07 14:17:49 +0200

    .

---
+++ frontend/src/features/header/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <section>
+        
+            <ul>
+                <li>Summary</li>
+                <li>Skills</li>
+                <li>Projects</li>
+            </ul>
+
+        </section>
+    )
+}
+
+export default Header
---
+++ frontend/src/features/pages/Layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Header } from '../header/index';
+
+function Layout() {
+    return (
+        <>
+            {/* Navigation */}
+            <Header />
+            {/* Screen */}
+            <section></section>
+        </>
+    )
+}
+
+export default Layout
